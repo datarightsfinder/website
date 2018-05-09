84947c42bfcfe327fc4b866f82686e0101b87fa8
Ian Hutchinson <ian@ihut.cc>
2018-05-09 10:34:27 +0100

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,41 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var gutil = require('gulp-util');
-var nodemon = require('gulp-nodemon');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var pump = require('pump');
-var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss'
-];
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const nodemon = require('nodemon');
+const log = require('fancy-log');
 
-gulp.task('default', ['sass', 'js', 'nodemon', 'watch']);
-gulp.task('deploy', ['sass', 'js']);
-
-gulp.task('watch', function() {
-  gulp.watch('assets/sass/**/*.scss', ['sass']);
-  gulp.watch('assets/js/*.js', ['js']);
+gulp.task("sass", function(cb) {
+  return gulp.src('assets/sass/app.scss')
+  .pipe(sass({
+    style: 'compressed',
+    loadPath: [
+      'bower_components/foundation/scss'
+    ]
+  }).on('error', function(err) {
+    log(err);
+  }))
+  .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('nodemon', function() {
+gulp.task("js", function(cb) {
+  pump([
+    gulp.src("assets/js/*.js"),
+    uglify(),
+    gulp.dest("public/js")
+  ], cb);
+});
+
+gulp.task("watch", function() {
+  gulp.watch('assets/sass/*.scss', gulp.parallel("sass"));
+  gulp.watch('assets/js/*.js', gulp.parallel("js"));
   nodemon({
     script: 'index.js',
-    ext: 'html yaml js'
+    ext: '',
+    ignore: ['public/*']
   });
 });
 
-gulp.task('sass', function() {
-  return gulp.src('assets/sass/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('public/css'));
-});
+gulp.task("default", gulp.parallel("sass", "js", "watch"));
 
-gulp.task('js', function (cb) {
-  pump([
-    gulp.src('assets/js/*.js'),
-    uglify(),
-    gulp.dest('public/js')
-  ], cb);
-});
+gulp.task("deploy", gulp.parallel("sass", "js"));
