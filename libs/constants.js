9423ed5ea9814ce48696c8e1b705e6af2efbad3a
Ian Hutchinson <ian@ihut.cc>
2018-06-05 00:38:04 +0100

    Create constants file

---
+++ libs/constants.js
@@ -0,0 +1,65 @@
+class Constants {
+  static getSpecialCategories() {
+    return [
+      "race",
+      "ethnic_origin",
+      "politics",
+      "religion",
+      "trade_union_membership",
+      "genetics",
+      "biometrics",
+      "health",
+      "sex_life",
+      "sexual_orientation"
+    ]
+  }
+
+  static getECAdequacyDecisionCountries() {
+    return [
+      "ad",
+      "ar",
+      "ca",
+      "il",
+      "nz",
+      "ch",
+      "uy"
+    ]
+  }
+
+  static getEEACountries() {
+    return [
+      "au",
+      "be",
+      "bg",
+      "cz",
+      "cy",
+      "dk",
+      "ee",
+      "fi",
+      "fr",
+      "de",
+      "gr",
+      "hu",
+      "is",
+      "ie",
+      "it",
+      "lv",
+      "li",
+      "lt",
+      "lu",
+      "mt",
+      "nl",
+      "no",
+      "pl",
+      "pt",
+      "ro",
+      "sk",
+      "si",
+      "es",
+      "se",
+      "gb"
+    ]
+  }
+}
+
+module.exports = Constants;
