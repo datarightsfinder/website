5a17722ed6f5a5c4e4cf23edb5f70047c13c04e0
Ian Hutchinson <ian@ihut.cc>
2018-06-19 14:05:32 +0100

    Prototype version of shortcuts

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,31 @@ $(function() {
 
     $('.small-search').fadeIn(250);
   });
+
+  // SHORTCUTS MENU
+  $('.shortcuts ul').hide();
+
+  $('.button-shortcut').click(function(e) {
+    e.preventDefault();
+
+    $('.shortcuts ul').toggle();
+
+    $(this).toggleClass('active');
+  });
+
+  $('.shortcuts ul li a').click(function(e) {
+    e.preventDefault();
+
+    var right = $(this).attr('data-right');
+
+    $('.shortcuts ul').hide();
+    $('.button-shortcut').removeClass('active');
+
+    $('.shortcut-info').hide();
+    $('#shortcut-' + right).fadeIn(250);
+  });
+
+  // $(window).on('resize', function() {
+  //   $('.')
+  // });
 });
