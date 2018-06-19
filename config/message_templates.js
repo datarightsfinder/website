e77201f6a1b2f39755816995c2e209e8ea506fde
Ian Hutchinson <ian@ihut.cc>
2018-06-19 19:40:57 +0100

    Message templates

---
+++ config/message_templates.js
@@ -0,0 +1,12 @@
+// In this file you can define the message templates shown on an organisation
+// page. To add a line break, use \n\n.
+
+module.exports = {
+  access: `this is a line about access\n\nthis is another line about access`,
+  rectification: `this is a line about rectification`,
+  erasure: `this is a line about erasure`,
+  restrict_processing: `this is a line about restrict_processing`,
+  data_portability: `this is a line about data_portability`,
+  object: `this is a line about object`,
+  automated_decision_making: `this is a line about automated_decision_making`,
+};
