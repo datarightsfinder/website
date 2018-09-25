76949962ea24bfb4b0c37e485d6d8807577dcb10
Ian Hutchinson <ian@ihut.cc>
2018-09-25 16:46:36 +0100

    Change how Sequelize connects to Postgres

--- config/config.js
+++ config/config.js
@@ -1,26 +1,25 @@
-let isSSLEnabled = false;
-
-if (process.env.NODE_ENV === 'production') {
-  isSSLEnabled = true;
-}
+require('dotenv').config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: isSSLEnabled,
+      ssl: false,
     },
   },
   test: {
-    dialect: 'sqlite',
-    storage: 'test/test.db',
+    url: `${process.env.DATABASE_URL}_test`,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false,
+    },
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: isSSLEnabled,
+      ssl: true,
     },
   },
 };
